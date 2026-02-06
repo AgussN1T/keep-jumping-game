5a46d51fc53bf38c00bfb4e2e5cd0d6d96db5d68
AgussN1T <agustinandresrivero@gmail.com>
2026-02-05 21:30:42 -0300

    cambios en config y generador de plataformas

--- src/game.js
+++ src/game.js
@@ -11,7 +11,7 @@ class GameScene extends Phaser.Scene {
     preload() {
 
         this.load.image('plataforma', 'assets/plataforma.png');
-        this.load.image('plataforma2', 'assets/plataforma2.png');
+        this.load.image('plataformaInicial', 'assets/plataforma2.png');
 
         this.load.spritesheet(
             'jugador', 'assets/jugador.png',
@@ -21,18 +21,22 @@ class GameScene extends Phaser.Scene {
 
     create() {
 
-        
+        this.time.addEvent({
+            delay: 20000,
+            callback: this.destroyPlataformaInicial,
+            callbackScope: this,
+        });
 
 
         this.time.addEvent({
-            delay: 1200,
+            delay: 1500,
             callback: this.spawnPlataforma,
             callbackScope: this,
             loop: true
         });
 
 
-        this.jugador = this.physics.add.sprite(180, 550, 'jugador').setOrigin(0, 1).setCollideWorldBounds(true)
+        this.jugador = this.physics.add.sprite(180, 450, 'jugador').setOrigin(0, 1).setCollideWorldBounds(true)
             .setGravityY(300).setSize(12, 16).setOffset(11, 16)
 
 
@@ -41,15 +45,17 @@ class GameScene extends Phaser.Scene {
             immovable: true
         });
 
-/*         this.physics.world.createDebugGraphic();
+        /*         this.physics.world.createDebugGraphic();
+        
+                this.jugador.body.debugShowBody = true;
+                this.jugador.body.debugBodyColor = 0xff0000; */
 
-        this.jugador.body.debugShowBody = true;
-        this.jugador.body.debugBodyColor = 0xff0000; */
 
+        // this.jugador.setScale(2)
 
-        this.plataforma = this.physics.add.staticSprite(180, 560, 'plataforma2')
+        this.plataformaInicial = this.physics.add.staticSprite(180, 460, 'plataformaInicial')
 
-        this.physics.add.collider(this.jugador, this.plataforma)
+        this.physics.add.collider(this.jugador, this.plataformaInicial)
 
         this.physics.add.collider(this.jugador, this.plataformas)
 
@@ -57,11 +63,25 @@ class GameScene extends Phaser.Scene {
 
         this.keys = this.input.keyboard.createCursorKeys()
 
-        this.jugador.anims.play('jugador-idle', true)
+        // this.jugador.anims.play('jugador-idle', true)
 
         this.jugador.body.checkCollision.up = false;
         this.jugador.body.checkCollision.left = false;
         this.jugador.body.checkCollision.right = false;
+
+
+        /* const esTouch = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
+        if (esTouch) {
+            console.log('Dispositivo táctil');
+        } */
+
+        /* if (navigator.userAgentData) {
+            const isMobile = navigator.userAgentData.mobile;
+            console.log(isMobile ? "Estás en un móvil" : "Estás en PC");
+        } */
+
+
 
     }
 
@@ -81,16 +101,33 @@ class GameScene extends Phaser.Scene {
 
     spawnPlataforma() {
 
-        const x = Phaser.Math.Between(40, this.scale.width - 40);
+        // const x = Phaser.Math.Between(40, this.scale.width - 40);
+        const x = Math.round(
+            Phaser.Math.Between(40, this.scale.width - 40)
+        );
+
         const y = -20;
-        const velocidad = Phaser.Math.Between(30, 60);
+
+        const velocidad = Phaser.Math.Between(8, 16) * 4;
+        // const velocidad = Phaser.Math.Between(15, 50);
 
         const plataforma = this.plataformas.create(x, y, 'plataforma');
+        
+        plataforma.preUpdate = function () {
+            this.x = Math.round(this.x);
+            this.y = Math.round(this.y);
+        };
 
         plataforma
             .setVelocityY(velocidad)
-            .setOrigin(0.5, 0.5);
+            .setOrigin(0.5, 1);
 
+    }
+
+    destroyPlataformaInicial() {
+        if (this.plataformaInicial) {
+            this.plataformaInicial.destroy();
+        }
     }
 
 
@@ -104,6 +141,10 @@ const config = {
     type: Phaser.AUTO,
     width: 360,
     height: 600,
+    pixelArt: true,
+    render: {
+        roundPixels: true
+    },
     backgroundColor: '#44afd6',
     parent: 'game',
     physics: {
